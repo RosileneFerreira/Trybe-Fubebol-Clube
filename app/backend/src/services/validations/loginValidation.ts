ad8e78e619139c5efec7924bba929501e6e284f6
Rosilene Ferreira <rosilenesanferr@gmail.com>
2023-01-21 23:10:20 -0300

    Não permite acesso sem informar uma senha

--- app/backend/src/services/validations/loginValidation.ts
+++ app/backend/src/services/validations/loginValidation.ts
@@ -2,9 +2,9 @@ import { Request, Response, NextFunction } from 'express';
 import { ILogin } from '../../interfaces/login.interface';
 
 const loginValidation = (req: Request, res: Response, next: NextFunction) => {
-  const { email } : ILogin = req.body;
+  const { email, password } : ILogin = req.body;
 
-  if (!email) return res.status(400).json({ message: 'All fields must be filled' });
+  if (!email || !password) return res.status(400).json({ message: 'All fields must be filled' });
 
   next();
 };
