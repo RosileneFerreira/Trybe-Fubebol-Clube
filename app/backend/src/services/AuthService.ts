15e46d4475375a8a4e0254ce8e1572c01396eda4
Rosilene Ferreira <rosilenesanferr@gmail.com>
2023-01-22 12:42:03 -0300

    Não permite acesso com um email ou senha inválido

--- app/backend/src/services/AuthService.ts
+++ app/backend/src/services/AuthService.ts
@@ -3,21 +3,30 @@ import User from '../database/models/User';
 import GenerateToken from '../utils/jwt';
 import { ILogin } from '../interfaces/login.interface';
 import HttpException from '../utils/http.exception';
+import loginValidation from './validations/loginValidation';
 
 export default class AuthService {
   public userModel = User;
   public validToken = new GenerateToken();
 
   public async login(loginBody: ILogin) {
+    const { email, password } = loginBody;
+    loginValidation(loginBody);
     const user = await this.userModel.findOne({ where: { email: loginBody.email } });
 
+    const unauthorized = 'Incorrect email or password';
+
     if (!user) {
-      throw new HttpException(401, 'Incorrect email or password');
+      throw new HttpException(401, unauthorized);
     }
 
     const validPassword = compareSync(loginBody.password, user.password);
     if (!validPassword) {
-      throw new HttpException(401, 'Incorrect email or password');
+      throw new HttpException(401, unauthorized);
+    }
+
+    if (loginBody.email !== email || loginBody.password !== password) {
+      throw new HttpException(401, unauthorized);
     }
 
     const token = this.validToken.token(user);
--- app/backend/src/services/validations/loginValidation.ts
+++ app/backend/src/services/validations/loginValidation.ts
@@ -1,11 +1,25 @@
-import { Request, Response, NextFunction } from 'express';
-import { ILogin } from '../../interfaces/login.interface';
+import Joi = require('joi');
 
-const loginValidation = (req: Request, res: Response, next: NextFunction) => {
-  const { email, password } : ILogin = req.body;
+import HttpException from '../../utils/http.exception';
 
-  if (!email || !password) return res.status(400).json({ message: 'All fields must be filled' });
+const REQUIRED = 'All fields must be filled';
 
-  next();
-};
-export default loginValidation;
+const loginSchema = Joi.object({
+  email: Joi.string().email().required().messages({
+    'any.required': REQUIRED,
+    'string.empty': REQUIRED,
+    'string.email': 'Incorrect email or password',
+  }),
+  password: Joi.string().min(7).required().messages({
+    'any.required': REQUIRED,
+    'string.empty': REQUIRED,
+  }),
+});
+
+export default function loginValidation(login: object) {
+  const { error, value } = loginSchema.validate(login, { convert: false });
+  if (error) {
+    throw new HttpException(400, error.message);
+  }
+  return { value };
+}
