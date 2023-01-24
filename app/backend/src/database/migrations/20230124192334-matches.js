bbb0a9eeb30f912c31a2c4c7545b8e36ab60bf38
Rosilene Ferreira <rosilenesanferr@gmail.com>
2023-01-24 17:07:12 -0300

    Migration e model para a tabela de matches

---
+++ app/backend/src/database/migrations/20230124192334-matches.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      homeTeamId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'home_team_id'
+      },
+      homeTeamGoals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'home_team_goals'
+      },
+      awayTeamId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'away_team_id'
+      },
+      awayTeamGoals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'away_team_goals'
+      },
+      inProgress: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        field: 'in_progress'
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('matches');
+  }
+};
---
+++ app/backend/src/database/models/Match.ts
@@ -0,0 +1,52 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+
+class Match extends Model {
+  id!: number;
+  homeTeamId!: number;
+  homeTeamGoals!: number;
+  awayTeamId!: number;
+  awayTeamGoals!: number;
+  inProgress!: boolean;
+}
+
+Match.init({
+  id: {
+    type: INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  homeTeamId: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'home_team_id',
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'home_team_goals',
+  },
+  awayTeamId: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'away_team_id',
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'away_team_goals',
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+    field: 'in_progress',
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+export default Match;
